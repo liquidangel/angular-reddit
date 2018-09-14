835923cc0cf35548896f4e76e796eba2afec4fad
liquidangel <paolo.pasia@gmail.com>
2018-09-15 01:42:19 +0800

    Added Article and Vendor components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-reddit';
+  articles: Article[];
+
+  constructor(){
+    this.articles = [ 
+      new Article("Angular 2", "http://angular.io", 10),
+      new Article("Angular 3", "http://angular.io", 10),
+      new Article("Angular 4", "http://angular.io", 10),
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    return false;
+  }
 }
